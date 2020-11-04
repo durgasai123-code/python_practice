54adcaad2bb0a715b3c681cb4d15c0e8e1822c8a
durga <durgasai831@gmail.com>
2020-11-04 22:21:23 +0530

    new line added

--- avg.py
+++ avg.py
@@ -1,1 +1,10 @@
-print("hello")
+def avg():
+    n = int(input("how many numbers:"))
+    total = 0
+    for i in range(n):
+        x = int(input("enter any number:"))
+        total += x
+    average = total/n
+    return average
+x = avg()
+print(x)
